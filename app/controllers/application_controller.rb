30127ce4ab8b4cc43d98eb900f494fbecd920dcb
Ignacio Moran <moran.ua@gmail.com>
2021-07-22 20:13:55 -0400

    devise install failed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    
+  before_action :authenticate_user!
+
+  def authorize_admin!
+    unless current_user.admin?
+        redirect_to root_path, notice: "You don't have clearance to access this section"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :views
+  
   resources :strains
   resources :wines
+
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+ }
+
 
   root 'wines#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
